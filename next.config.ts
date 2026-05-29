8532105c91f55e3a00f453ad14094683c9bee7d1
ajaib1990 <ajaib.online@gmail.com>
2026-05-29 16:15:12 +0300

    another issue

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",  // <=== enables static exports
+  basePath: "/leko", // <=== Add your repository name here
   // reactStrictMode: true,
   images: {
     unoptimized: true, // Add this line
